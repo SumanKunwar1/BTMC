5aa94061b8010962765fa98bb2824dde5c6860fa
Suman Kunwar <sumankunwar755@gmail.com>
2026-03-11 13:50:10 +0545

    update the navigation bar

--- Frontend/src/config/navigation.ts
+++ Frontend/src/config/navigation.ts
@@ -1,8 +1,9 @@
 export const NavItems = [
   { path: '/', label: 'Home' },
   { path: '/about', label: 'About' },
+  { path: '/events', label: 'Retreats' },
   { path: '/teachings', label: 'Teachings' },
   { path: '/tours', label: 'Tours' },
-  { path: '/events', label: 'Retreats' },
+  { path: '/donate', label: 'Donars' },
   { path: '/contact', label: 'Contact' },
 ] as const;
